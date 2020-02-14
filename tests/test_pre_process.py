e7bd682b46081471924995f118da2990413a4ec7
sahilchutani91 <sc4617@columbia.edu>
2020-02-13 22:31:05 -0500

    Travis CI

--- tests/test_pre_process.py
+++ tests/test_pre_process.py
@@ -6,3 +6,11 @@ from utils.pre_process import *
 
 def test_clean_text():
     pass
+def test_tokenize_text():
+    pass
+
+def test_replace_token_with_index():
+    pass
+
+def test_pad_sequence():
+    pass
